34a1624234319786bfdd4d1a46ed1ff9b50dbb49
liyujian3225 <icanflyanywhere@126.com>
2023-01-16 18:35:17 +0800

    initCode

---
+++ src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="HomeView" @click="increment">
+    {{ state.count }}
+  </div>
+</template>
+<script setup>
+import { reactive } from 'vue';
+
+const state = reactive({ count: 0 });
+
+function increment() {
+  state.count++;
+}
+</script>
