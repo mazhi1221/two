0b9bffa06ae2d95c8bdd5041516333308bd74eae
liyujian3225 <icanflyanywhere@126.com>
2023-01-17 17:38:38 +0800

    引入ElementPlus

---
+++ src/components/header/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="header">
+    <div class="contain">
+      ffffffffffffffffff
+    </div>
+  </div>
+</template>
+<script setup>
+
+</script>
+<style lang="scss" scoped>
+div.header {
+  width: 100%;
+  height: 50px;
+  line-height: 50px;
+  font-size: 13px;
+  background: #444;
+  position: relative;
+  div.contain {
+    width: 100%;
+    min-width: 960px;
+    margin: 0 auto;
+    max-width: 1400px;
+  }
+}
+</style>
