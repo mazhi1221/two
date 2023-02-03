853c249e4476bd7d01dbeaeb6cff30a393b3bf77
liyujian3225 <icanflyanywhere@126.com>
2023-02-03 23:39:21 +0800

    新增暗黑模式切换

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -1,51 +1,65 @@
 <template>
-  <div class="header">
-    <div class="logoBox">
-      <div class="img">
-        <img src="../../assets/img/logo.png" alt="logo" />
-      </div>
-      <h2>禅</h2>
+    <div class="header">
+        <div class="logoBox">
+            <div class="img">
+                <img src="../../assets/img/logo.png" alt="logo" />
+            </div>
+            <h2>禅</h2>
+        </div>
+        <div class="loginInfo">
+            <el-switch 
+                v-model="isDark"
+                inline-prompt
+                :active-icon="Moon"
+                :inactive-icon="Sunny"
+                style="--el-switch-on-color: #13ce66; --el-switch-off-color: #ff4949"
+            />
+        </div>
     </div>
-    <div class="loginInfo">
-      <span>StephenLee</span>
-    </div>
-  </div>
 </template>
 <script setup>
+import { useDark } from '@vueuse/core'
+import { Sunny, Moon } from '@element-plus/icons-vue'
+
+const isDark = useDark({
+    valueDark: 'dark',
+    valueLight: 'light',
+})
+
 </script>
 <style lang="scss" scoped>
 div.header {
-  width: 100%;
-  height: 100%;
-  padding: 0 22px;
-  box-sizing: border-box;
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  div.logoBox {
+    width: 100%;
+    height: 100%;
+    padding: 0 22px;
+    box-sizing: border-box;
     display: flex;
+    justify-content: space-between;
     align-items: center;
-    div.img {
-      width: 31px;
-      height: 31px;
-      margin-right: 12px;
+    div.logoBox {
+        display: flex;
+        align-items: center;
+        div.img {
+            width: 31px;
+            height: 31px;
+            margin-right: 12px;
 
-      img {
-        width: 100%;
-      }
-    }
+            img {
+                width: 100%;
+            }
+        }
 
-    h2 {
-      font-size: 20px;
-      color: #fff;
-      font-weight: normal;
+        h2 {
+            font-size: 20px;
+            color: #fff;
+            font-weight: normal;
+        }
     }
-  }
-  div.loginInfo {
-    display: flex;
-    align-items: center;
-    font-size: 16px;
-    color: #fff;
-  }
+    div.loginInfo {
+        display: flex;
+        align-items: center;
+        font-size: 16px;
+        color: #fff;
+    }
 }
 </style>
--- src/views/deviceManagement/index.vue
+++ src/views/deviceManagement/index.vue
@@ -13,25 +13,106 @@
                         link
                         type="primary"
                         size="small"
-                        @click="handDetailleClick"
+                        @click="dialogVisible = true"
                         >Detail</el-button
                     >
                     <el-button link type="primary" size="small">Edit</el-button>
-                    <el-button link type="primary" size="small">delete</el-button>
+                    <el-button link type="primary" size="small"
+                        >delete</el-button
+                    >
                 </template>
             </el-table-column>
         </el-table>
+        <el-dialog
+            v-model="dialogVisible"
+            title="传感器详情"
+            width="50%"
+            :before-close="handleClose"
+        >
+            <el-row>
+                <el-col :span="16"
+                    ><div class="grid-content ep-bg-purple" />
+                    <el-table
+                        :data="sensorsTableData"
+                        @selection-change="handleSelectionChange"
+                        style="width: 100%"
+                    >
+                        <el-table-column type="selection" width="55" />
+                        <el-table-column
+                            type="index"
+                            label="序号"
+                            width="100"
+                        />
+                        <el-table-column prop="sensorsName" label="名称" />
+                        <el-table-column prop="sensorsStatus" label="状态" />
+                        <el-table-column prop="turnOnTime" label="在线时长" />
+                    </el-table>
+                </el-col>
+                <el-col :span="8"
+                    ><div class="grid-content ep-bg-purple-light" />
+                    <el-date-picker
+                        v-model="value1"
+                        type="datetime"
+                        placeholder="Select date and time"
+                    />
+                    <line-chart
+                        :params="chartdata"
+                        width="400px"
+                        height="300px"
+                    />
+                </el-col>
+            </el-row>
+
+            <template #footer>
+                <span class="dialog-footer">
+                    <el-button @click="dialogVisible = false">Cancel</el-button>
+                    <el-button type="primary" @click="dialogVisible = false">
+                        Confirm
+                    </el-button>
+                </span>
+            </template>
+        </el-dialog>
     </div>
 </template>
 <script setup>
-const tableData = [];
+import { ref } from 'vue'
+import { ElMessageBox } from 'element-plus'
+import lineChart from '/src/components/charts/lineChart.vue'
+const tableData = []
+const sensorsTableData = []
 for (let i = 0; i < 10; i++) {
     tableData.push({
-        deviceName: "路由器" + i,
-        deviceAddress: "192.168.40.174",
-        deviceStatus: "开启",
-        turnOnTime: "2023-03-02 15:00",
-        sensorsNum: "49",
+        deviceName: '路由器' + i,
+        deviceAddress: '192.168.40.174',
+        deviceStatus: '开启',
+        turnOnTime: '2023-03-02 15:00',
+        sensorsNum: '49'
     })
-} 
+}
+
+for (let i = 0; i < 5; i++) {
+    sensorsTableData.push({
+        sensorsName: '传感器' + i,
+        sensorsStatus: '开',
+        turnOnTime: '2023-03-02 15:00'
+    })
+}
+
+const dialogVisible = ref(false)
+const value1 = ref("")
+const handleClose = (done) => {
+    ElMessageBox.confirm('Are you sure to close this dialog?')
+        .then(() => {
+            done()
+        })
+        .catch(() => {
+            // catch error
+        })
+}
+const multipleSelection = ref([])
+const chartdata = ref([])
+const handleSelectionChange = (val) => {
+    multipleSelection.value = val
+    chartdata = val.map((item) => {})
+}
 </script>
