10b294e66449cb351ecf6adc82f3afe9fd013740
liyujian3225 <icanflyanywhere@126.com>
2023-03-20 18:59:40 +0800

    状态管理器，模块页面添加

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# vue3Project
+# drawerTool
 
 This template should help get you started developing with Vue 3 in Vite.
 
--- src/stores/user.js
+++ src/stores/user.js
@@ -1,14 +1,18 @@
-// stores/counter.js
-import { defineStore } from 'pinia'
+import { setItem, getItem, removeItem } from "@/utils/storage";
+import { defineStore } from 'pinia';
 
-export const useCounterStore = defineStore('counter', {
+//state 是 store 的数据 (data)，getters 是 store 的计算属性 (computed)，而 actions 则是方法 (methods)
+export const useUserStore = defineStore('user', {
   state: () => {
-    return { count: 0 }
+    return {
+      token: getItem("authorization")
+    }
   },
-  // 也可以这样定义
-  // state: () => ({ count: 0 })
+  getters: {
+    token_: (state) => state.token,
+  },
   actions: {
-    increment() {
+    setUser() {
       this.count++
     },
   },
