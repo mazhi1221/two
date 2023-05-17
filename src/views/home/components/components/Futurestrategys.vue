1782086ff2808c8cc2fde63ea84d033170515abc
baiyanjun <2306833669@qq.com>
2023-05-18 00:28:10 +0800

    abc

--- src/views/home/components/components/Futurestrategys.vue
+++ src/views/home/components/components/Futurestrategys.vue
@@ -1,3 +1,89 @@
+<<<<<<< HEAD
 <template>
   <div>123456</div>
 </template>
+=======
+<template>
+  <div class="bxo">
+    <div v-for="item in 2" :key="item" class="box">
+      <img :src="props.data[0].imgList[item - 1].url" alt="">
+      <div>
+        {{ props.data[0].imgList[item - 1].title }}
+      </div>
+    </div>
+
+    <ul class="ul">
+      <li v-for="(item, index) in props.data[1].imgList" :key="index">
+        <img :src="item.url" alt="">
+        <p>{{ item.title }}</p>
+        <div>{{ item.description }}</div>
+      </li>
+    </ul>
+  </div>
+</template>
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({ data: Array })
+console.log(props.data[1])
+</script>
+<style lang="scss" scoped>
+.bxo {
+  background-color: white;
+  padding-bottom: 30px;
+}
+
+.bxo>.box {
+  position: relative;
+  height: 556.46px;
+  img {
+    width: 100%;
+    height: 100%;
+    vertical-align: bottom;
+  }
+
+  div {
+    position: absolute;
+    left: 74.03px;
+    top: 450px;
+    height: 43px;
+    opacity: 1;
+    // font-family: 思源黑体;
+    font-size: 30px;
+    font-weight: bold;
+    letter-spacing: 0em;
+
+    color: #FFFFFF;
+  }
+}
+
+ul {
+  overflow: hidden;
+  margin-top: 30px;
+  li {
+    width: 25%;
+    float: left;
+    width: 341.37px;
+    margin:0 0 15px 15px;
+    img {
+      width: 100%;
+      height: 474.27px;
+      border-radius: 20px;
+    }
+    p{
+      font-size: 16px; 
+      color: #3D3D3D;
+      font-weight: 550;
+      padding: 0px 5px;
+    };
+    div {
+      color: #3D3D3D;
+      font-size: 16px;
+      font-weight: 350;
+      letter-spacing: 0em;
+      padding: 0px 5px;
+    }
+  }
+}
+</style>
+>>>>>>> 19f8d25... code
--- src/views/home/page/fashionTrend.vue
+++ src/views/home/page/fashionTrend.vue
@@ -1,12 +1,16 @@
 <template>
-  <div class="fashionTrend">
+  <div class="fashionTrend" >
     <div class="fashionTrendContain">
       <div class="category">
         <el-space :size="90">
           <div
             v-for="(item,index) in officialDocuments"
             :class="['tag', { active: officialActive === item.value }]"
+<<<<<<< HEAD
             @click="officialActive = item.value"
+=======
+            @click="TitleClick(item,index) "
+>>>>>>> 19f8d25... code
             :key="index"
           >
             <span class="title">{{ item.name }}</span>
@@ -36,32 +40,44 @@
 </template>
 <script setup>
 import TrendInfo from "../components/trendInfo.vue"
+<<<<<<< HEAD
 import MarketAnalysis from "../components/components/MarketAnalysis.vue";
+=======
+>>>>>>> 19f8d25... code
 import Futurestrategy from "../components/components/Futurestrategys.vue"
 import renderData from "./imageConfig.json";
 
 const officialActive = $ref("futureStrategy")
+const TitleClick = (item,index)=> {
+  console.log(item,index);
+  officialActive = item.value
+}
 const officialDocuments = [
   { name: "未来策略", value: "futureStrategy" },
   { name: "市场策略", value: "marketAnalysis" },
   { name: "调研设计", value: "researchDesign" },
   { name: "品牌解读", value: "brandInterpretation" },
 ]
-
 </script>
 <style lang="scss" scoped>
 div.fashionTrend {
   width: 100%;
   padding-top: 120px;
   padding-bottom: 120px;
+<<<<<<< HEAD
   background: #000000;
+=======
+  // background-color: white;
+>>>>>>> 19f8d25... code
   div.fashionTrendContain {
-    width: 1440px;
+    width: 1440px; // 全屏
     margin: 0 auto;
+    
     >div.category {
       display: flex;
       justify-content: center;
       margin-bottom: 64px;
+      
       div.tag {
         span.title {
           cursor: pointer;
